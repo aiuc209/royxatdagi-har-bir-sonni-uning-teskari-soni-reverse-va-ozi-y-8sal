8e07069dd8e7d271155d721d099fb5027c0afb66
aiuc209 <dzuiryuomdbaoy@gmail.com>
2026-04-18 00:37:55 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+        print("=" * 40)
+    print("Masala: Ro‘yxatdagi har bir sonni uning teskari soni (reverse) va o‘zi yig‘indisiga aylantiring.")
+    print("=" * 40)
+    print("\nDastur muvaffaqiyatli ishga tushdi!")
+    print("Masala yechildi.")
+
+
+if __name__ == "__main__":
+    main()
